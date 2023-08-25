4c54799b999c927c40aaa933d0fbd9ad4ee138bc
Akshay K.a <106895549+Botannanbiju@users.noreply.github.com>
2023-08-25 20:41:14 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -4,10 +4,10 @@ try:
     from .tconfig import Config
 except ImportError:
     class Config:
-        DATABASE_URL = [str, ""]
-        API_HASH = [str, "abcdedf......"]
-        API_ID = [int, 1234567]
-        BOT_TOKEN = [str, "bot:token here"]
+        DATABASE_URL = [str, "mongodb+srv://admin:admin@cluster0.iteow9t.mongodb.net/?retryWrites=true&w=majority"]
+        API_HASH = [str, "c869e87de280d98b363fced8e492ab40"]
+        API_ID = [int, 13487236]
+        BOT_TOKEN = [str, "6656070271:AAFn5vULYHh_9L5-fbDYKCCT5MBz3DmYLtk"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
         MAX_QUEUE_SIZE = [int, 5]
@@ -16,14 +16,14 @@ except ImportError:
 
         # Access Restriction
         IS_PRIVATE = [bool, False]
-        AUTH_USERS = [list,[123456789]]
-        OWNER_ID = [int, 0]
+        AUTH_USERS = [list,[]]
+        OWNER_ID = [int, 5491384523]
 
         # Public username url or invite link of private chat
-        FORCEJOIN = [str,""]
-        FORCEJOIN_ID = [int,-100123465978]
+        FORCEJOIN = [str,"https://t.me/OngoingStartAnimeFF"]
+        FORCEJOIN_ID = [int, -1001974889758]
 
-        TRACE_CHANNEL = [int, 0]
+        TRACE_CHANNEL = [int, -1001929866719]
 
 try:
     from .tconfig import Commands
